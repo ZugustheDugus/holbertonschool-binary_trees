ba216b927bc1aee0d8461e0d47b73d313e57aeca
ZugustheDugus <h1cooper2019@protonmail.com>
2022-08-04 23:17:14 -0500

    asdf

--- binary_trees.h
+++ binary_trees.h
@@ -1,8 +1,9 @@
-#ifndef BINARY_TREES
-#define BINARY_TREES
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -23,7 +24,6 @@ struct binary_tree_s
 typedef struct binary_tree_s binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
-
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
